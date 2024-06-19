b481340b1a544e890cdbdbfd7dc12c3960fcc6cc
James Luncan <jluncan@tql.com>
2024-06-19 14:04:30 -0400

    Add project files.

---
+++ MathLibrary/Program.cs
@@ -0,0 +1,20 @@
+﻿using MathLibrary2;
+using StringLib;
+
+namespace MathLibrary {
+    internal class Program {
+
+     
+        static void Main(string[] args) {
+
+            
+
+            var sl = new StringLibrary();
+            var newstr = sl.Concat("Hello," , "World");
+            Console.WriteLine(newstr);
+
+            var answer = MathLib.Add(11, 22);
+            Console.WriteLine(answer);
+        }
+    }
+}
---
+++ StringLib/StringLibrary.cs
@@ -0,0 +1,37 @@
+﻿
+
+using System.Data.SqlTypes;
+
+namespace StringLib {
+    public class StringLibrary {
+        public string Concat(string str1, string str2) {
+
+            return $"{str1} {str2}"; // could do str1 + " " + str2
+        }
+
+        public string ToUpperEveryOther(string str) {
+            var toUpper = true;
+            var converted = string.Empty;
+            foreach (var ch in str.ToCharArray()) {
+                if (!char.IsLetter(ch)) {
+                    converted += ch;
+                    continue;
+
+                }
+                if (toUpper) {
+                    converted += ch.ToString().ToUpper();
+                    toUpper = !toUpper;
+                } else {
+                    converted += ch.ToString().ToLower();
+                    toUpper = !toUpper;
+                }
+
+
+            }
+            return converted;
+        }
+    }
+}
+        
+    
+
